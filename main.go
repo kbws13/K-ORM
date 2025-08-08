177ffb1701c9b091dd199810f8d8ec7128aa8d5d
kbws13 <hsy040506@163.com>
2025-08-09 00:02:50 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func main() {
+	db, _ := sql.Open("sqlite3", "orm.db")
+	defer func() { _ = db.Close() }()
+	_, _ = db.Exec("drop table if exists User")
+	_, _ = db.Exec("CREATE TABLE User(Name text);")
+	result, err := db.Exec("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam")
+	if err == nil {
+		affected, _ := result.RowsAffected()
+		log.Println(affected)
+	}
+	row := db.QueryRow("SELECT Name FROM User LIMIT 1")
+	var name string
+	if err := row.Scan(&name); err == nil {
+		log.Println(name)
+	}
+}
